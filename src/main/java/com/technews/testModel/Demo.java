91e3682b61fc6ec99df4d1de04c3424649b2b508
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-05-29 17:15:05 -0400

    created demo class

--- src/main/java/com/technews/testModel/Demo.java
+++ src/main/java/com/technews/testModel/Demo.java
@@ -1,5 +1,7 @@
 package com.technews.testModel;
 
+import java.util.Objects;
+
 public class Demo {
     private String name;
     private int age;
@@ -24,4 +26,25 @@ public class Demo {
     public void setAge(int age) {
         this.age = age;
     }
-}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Demo demo = (Demo) o;
+        return age == demo.age && Objects.equals(name, demo.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Demo{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+};
